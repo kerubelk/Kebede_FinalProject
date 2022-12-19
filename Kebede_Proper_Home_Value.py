1bdf58c6a3891985105eae47cbbc0f4f02ca0637
kerubelk <110796409+kerubelk@users.noreply.github.com>
2022-12-18 23:48:59 -0500

    Update Kebede_Proper_Home_Value.py

--- Kebede_Proper_Home_Value.py
+++ Kebede_Proper_Home_Value.py
@@ -5,33 +5,13 @@ Navigator: None
 Assignment: Final Project
 Date: 11/29/2022
 
-Challenges Encountered: ~~~~~~~~~~~~~~~~~~~~~~~~
+Challenges Encountered: After the code for webscraping, which is(home_reccomendation) function is executed, the display does not stay on but closes. 
 """
+import math 
 
+  
 
-import sys
-import argparse
-import csv
-
-class value:
-
-    location = input('Where is the location of the house: ')
-
-    def __init__(self, location):
-        self.location = location  
-   
-   
-    def home_location(self.location):
-            '''
-            Args: 
-            location(str): The adress/city of the house
-            Function: 
-            This function takes the location/city entered and searches a database list of
-            homes in the entered location. 
-            '''
-            print(self.location)
-
-    def home_info(self, doors, windows, square_footage , age, estimate_price):
+def proper_value_calculation(doors, windows, square_footage, age):
             '''
             Args:
             doors(int): this holds the number of doors(rooms) in the house.
@@ -44,33 +24,23 @@ class value:
             and gives an estimate on the proper value of the home. 
             '''
             
-
-            per_squarefootage = 100
-            per_numofdoors = 1100
-            per_numofwindows = 840
-            per_year_for_age = 1470
-            doors = input('How many doors are in the house: ')
-            windows = input('How many windows are in the house: ')
-            square_footage = input('What is the square footage of the house: ')
-            age = input('How old is the house: ')
-
-            self.doors = doors
-            self.windows = windows
-            self.square_footage = square_footage
-            self.age = age
-
-            square_footage_price = square_footage * per_squarefootage 
-            doors_price = doors * per_numofdoors
-            windows_price = windows * per_numofwindows
-            house_years_price = age * per_year_for_age
-
-            estimate_price = square_footage + doors_price + windows_price + house_years_price
-
-            return estimate_price
+            #The prices for the attributes of the house
+            square_footage_price = square_footage * 100 
+            doors_price = doors * 1100
+            windows_price = windows * 840
+            house_years_price = age * 1470
+            
+            #calculation for the estimation of the home value
+            estimate_price = square_footage_price + doors_price + windows_price + house_years_price
+            print("estimate price: ", estimate_price)
+           
+            return estimate_price   #return the estimate price for the house
 
             
 
-    def home_recommendation(self, location, price, recommendation):
+            
+
+def home_recommendation():
             '''
             Args:
             location(str): holds the desired location/city
@@ -80,35 +50,53 @@ class value:
             This function takes the desired location/city, prefered price range and mathes it with location and price ranges in a database
             and returns the results. 
             '''
-            price = input('Enter your preferred price range: ')
-            print(price)
-            #working on this function
+            #import the proper modules for selenium
+            from selenium import webdriver
+            from selenium.webdriver.support.ui import Select
+            from selenium.webdriver.common.by import By
             
-    def main(estimate_price, recommendation):
-            '''
-            Args: 
-            estimate_price(float): holds the estimate price of the home
-            recommendation(list): holds a list of recommended homes from database
-            Function:
-            This function returns the estimate price after the calculation and the recommended homes 
-            in the area chosen by the user. 
-            '''
-            #Code goes here
-
-    def parse_args(args_list):
-            '''Takes a list of strings from the command prompt and passes them through as arguments
-
-            Args:
-                args_list(list): the list of strings from the command prompt
-            Returns:
-                args (ArgumentParser)
+            #url for the website
+            website = "https://www.newhomesource.com/mls_com"
             
-            '''
+            path = r'"\Users\kerub\Downloads\chromedriver_win32.zip"'
+            #the prefered location for the house
+            location = input("Enter a location: ")
+            #variable that connects between the chrome path and website
+            driver = webdriver.Chrome(path)
+            #fetch the website for the new homes list
+            driver.get(website)
 
-            parser = argparse.ArgumentParser()
-            parser.add_argument('estimate_price', type = float, help = 'holds the estimate price of a home based on the attributes entered.')
-            parser.add_argument('recommendation', type = list, help = 'holds the list of recommended homes in the locatin entered.')
+            #finds the element where the location can be entered in the website
+            search = driver.find_element(By.NAME, "SearchText")
 
-            args = parser.parse_args(args_list)
+            #sends the location entered by the user to the website
+            search.send_keys(location)
 
-            return args
+            #finds the element of the search button
+            search_button = driver.find_element(By.ID, "HomeSearchBtn")
+            #clicks the search button
+            search_button.click()
+            
+            
+def main():
+        #gets the attributes of the house from the user
+        doors = int(input('How many doors are in the house: '))
+        windows = int(input('How many windows are in the house: '))
+        square_footage = int(input('What is the square footage of the house: '))
+        age = int(input('How old is the house: '))
+        
+        #calls the function to do the calculation of the estimation price for the house
+        proper_value_calculation(doors, windows, square_footage, age)
+
+        #asks the user if they would like to look at homes list in a specific location
+        reccomendation = input("Would you like to search for homes in you're prefered location: ")
+        
+        #if the user enters yes, home_recommendation function is called
+        if (reccomendation == 'yes'):
+            home_recommendation()
+        else:
+            print("Thank you for using my program:") #if the user enters no, the program ends
+            
+            
+if __name__ == "__main__":
+    main()
